e9bfc33a9d3ee3c46c8f5fd70fdfb2dade0fcbc6
set830 <SET@Shawns-iMac.local>
2015-11-14 21:26:33 -0600

    HW complete

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,6 @@
 class Character < ActiveRecord::Base
   validates :movie_id, :presence => true
   validates :actor_id, :presence => true
+
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,4 +8,7 @@ class Movie < ActiveRecord::Base
   belongs_to :director
   has_many :characters
 
+  has_many :characters
+  has_many :actors, :through => :characters
+
 end
